a78a9cca7239344dda166891a72a4e55dcc423ad
Hanny0508 <166279805+Hanny0508@users.noreply.github.com>
2026-04-02 16:33:07 +0800

    Add files via upload

---
+++ zero-shot-GLS/scripts/download_models.py
@@ -0,0 +1,32 @@
+"""
+Download the models and tokenizers from HuggingFace.
+"""
+import os
+import os.path as osp
+
+os.environ["HF_HOME"] = f"{osp.dirname(__file__)}/../tmp_saves/hg_cache"
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+if __name__ == "__main__":
+    # GPT2
+    model_name = "gpt2-medium"
+    print(f"Downloading {model_name}")
+    AutoTokenizer.from_pretrained(model_name)
+    AutoModelForCausalLM.from_pretrained(
+        model_name,
+        resume_download=True,
+        max_memory={"cpu": "64GiB"},
+    )
+    # LlaMa2
+    model_name = "TheBloke/Llama-2-7B-chat-GPTQ"
+    print(f"Downloading {model_name}")
+    AutoTokenizer.from_pretrained(model_name)
+    AutoModelForCausalLM.from_pretrained(
+        model_name,
+        resume_download=True,
+        trust_remote_code=False,
+        # revision="gptq-4bit-32g-actorder_True",
+        revision="main",
+        max_memory={"cpu": "64GiB"},
+    )
